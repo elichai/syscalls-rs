c358dc7c5afb715197d4db58955d93c4653dd22f
Elichai Turkel <elichai.turkel@gmail.com>
2019-10-16 16:08:19 +0300

    Add failure test case

--- src/lib.rs
+++ src/lib.rs
@@ -14,7 +14,7 @@ use std::os::raw::{c_long, c_ulong};
 pub unsafe fn write(fd: RawFd, msg: &[u8]) -> Result<usize, io::Error> {
     let res = syscall!(Syscalls::Write as c_long, c_long::from(fd), msg.as_ptr() as c_long, msg.len() as c_long);
     if res < 0 {
-        Err(io::Error::from_raw_os_error(res as i32))
+        Err(io::Error::from_raw_os_error(-res as i32))
     } else {
         Ok(res as usize)
     }
@@ -22,11 +22,28 @@ pub unsafe fn write(fd: RawFd, msg: &[u8]) -> Result<usize, io::Error> {
 
 
 #[cfg(test)]
-#[test]
-fn test_print() {
-    use std::os::unix::io::AsRawFd;
-    let msg = "Hello World\n";
-    let res = unsafe { write(io::stdout().as_raw_fd(), msg.as_bytes()) };
-    println!("res: {:?}", res);
-    assert!(res.is_ok());
+mod tests {
+    use super::write;
+    use std::io;
+
+    #[test]
+    fn test_print() {
+        use std::os::unix::io::AsRawFd;
+        let msg = "Hello World\n";
+        let res = unsafe { write(io::stdout().as_raw_fd(), msg.as_bytes()) };
+        println!("res: {:?}", res);
+        assert!(res.is_ok());
+    }
+
+    #[test]
+    fn test_fail() {
+        let msg = "Hello World\n";
+        let res = unsafe { write(500, msg.as_bytes()) };
+        println!("res: {:?}", res);
+        assert!(res.is_err());
+        let err = res.unwrap_err();
+        assert_eq!(err.kind(), io::ErrorKind::Other);
+        assert_eq!(err.to_string(), "Bad file descriptor (os error 9)");
+    }
+
 }
