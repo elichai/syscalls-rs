23f3d7ff8c793375ccd69eb5f3c3eac7800cee9e
David Craven <david@craven.ch>
2019-11-07 11:28:20 +0100

    Improve socket api.

--- src/socket.rs
+++ src/socket.rs
@@ -2,10 +2,6 @@ use crate::arch::Syscalls;
 use crate::close;
 use crate::{result, syscall};
 use core::mem::size_of_val;
-use libc::{
-    sockaddr, socklen_t, AF_INET, AF_INET6, SOCK_CLOEXEC, SOCK_DGRAM, SOCK_NONBLOCK, SOCK_RAW,
-    SOCK_RDM, SOCK_SEQPACKET, SOCK_STREAM,
-};
 use std::net::SocketAddr;
 use std::os::unix::io::{AsRawFd, RawFd};
 use std::{
@@ -36,58 +32,102 @@ impl Drop for Fd {
     }
 }
 
-fn socket_addr(addr: SocketAddr) -> (*const sockaddr, socklen_t) {
+fn socket_addr(addr: &SocketAddr) -> (*const libc::sockaddr, libc::socklen_t) {
     match addr {
         SocketAddr::V4(ref addr) => (
-            addr as *const _ as *const sockaddr,
-            size_of_val(addr) as socklen_t,
+            addr as *const _ as *const libc::sockaddr,
+            size_of_val(addr) as libc::socklen_t,
         ),
         SocketAddr::V6(ref addr) => (
-            addr as *const _ as *const sockaddr,
-            size_of_val(addr) as socklen_t,
+            addr as *const _ as *const libc::sockaddr,
+            size_of_val(addr) as libc::socklen_t,
         ),
     }
 }
 
-#[derive(Clone, Copy, Debug)]
-#[repr(i32)]
-pub enum SockType {
-    Stream = SOCK_STREAM,
-    Datagram = SOCK_DGRAM,
-    SeqPacket = SOCK_SEQPACKET,
-    Raw = SOCK_RAW,
-    Rdm = SOCK_RDM,
+/// Constants used to specify the protocol family to be used in [`socket`](fn.socket.html).
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+pub enum AddressFamily {
+    /// Local communication ([`unix(7)`](http://man7.org/linux/man-pages/man7/unix.7.html)).
+    Unix = libc::AF_UNIX as isize,
+    /// IPv4 internet protocols ([`ip(7)`](http://man7.org/linux/man-pages/man7/ip.7.html)).
+    Inet = libc::AF_INET as isize,
+    /// IPv6 internet protocols ([`ipv6(7)`](http://man7.org/linux/man-pages/man7/ipv6.7.html)).
+    Inet6 = libc::AF_INET6 as isize,
 }
 
-#[derive(Clone, Copy)]
-pub struct SockFlags(i32);
+/// Constants used to specify the communication semantics when creating a
+/// socket with [`socket`](fn.socket.html)
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+pub enum SockType {
+    /// Provides sequenced, reliable, two-way, connection based byte streams.
+    /// An out-of-band data transmission mechanism may be supported.
+    Stream = libc::SOCK_STREAM as isize,
+    /// Supports datagrams - connectionless, unreliable messages of a fixed
+    /// maximum length.
+    Datagram = libc::SOCK_DGRAM as isize,
+    /// Provides a sequenced, reliable, two-way connection based data
+    /// transmission path for datagrams of fixed maximum length; a consumer is
+    /// required to read an entire packet with each input system call.
+    SeqPacket = libc::SOCK_SEQPACKET as isize,
+    /// Provides raw network protocol access.
+    Raw = libc::SOCK_RAW as isize,
+    /// Provides a reliable datagram layer that does not guarantee ordering.
+    Rdm = libc::SOCK_RDM as isize,
+}
+
+/// Constants used in [`socket`](fn.socket.html) and [`socketpair`](fn.socketpair.html)
+/// to specify the protocol to use.
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+pub enum SockProtocol {
+    /// TCP protocol ([ip(7)](http://man7.org/linux/man-pages/man7/ip.7.html)).
+    Tcp = libc::IPPROTO_TCP as isize,
+    /// UDP protocol ([ip(7)](http://man7.org/linux/man-pages/man7/ip.7.html)).
+    Udp = libc::IPPROTO_UDP as isize,
+}
+
+/// Additional socket options.
+#[derive(Clone, Copy, Eq, PartialEq)]
+pub struct SockFlags(isize);
 
 impl SockFlags {
+    /// Creates new `SocketFlags`.
     pub fn new() -> Self {
         Self(0)
     }
 
+    /// Set non-blocking mode on the new socket.
     pub fn nonblock(mut self) -> Self {
-        self.0 |= SOCK_NONBLOCK;
+        self.0 |= libc::SOCK_NONBLOCK as isize;
         self
     }
 
+    /// Set close-on-exec on the new descriptor.
     pub fn cloexec(mut self) -> Self {
-        self.0 |= SOCK_CLOEXEC;
+        self.0 |= libc::SOCK_CLOEXEC as isize;
         self
     }
 }
 
+impl core::fmt::Debug for SockFlags {
+    fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
+        f.debug_struct("SockFlags")
+            .field("NONBLOCK", &(self.0 & libc::SOCK_NONBLOCK as isize > 0))
+            .field("CLOEXEC", &(self.0 & libc::SOCK_CLOEXEC as isize > 0))
+            .finish()
+    }
+}
+
 #[inline]
-pub fn socket(addr: SocketAddr, sock: SockType, flags: SockFlags) -> io::Result<Fd> {
+pub fn socket(
+    domain: AddressFamily,
+    sock: SockType,
+    flags: SockFlags,
+    protocol: Option<SockProtocol>,
+) -> io::Result<Fd> {
     // flags only supported by kernel >= 2.6.27
-    let domain = match addr {
-        SocketAddr::V4(_) => AF_INET,
-        SocketAddr::V6(_) => AF_INET6,
-    };
-    let ty = sock as i32 | flags.0;
-    // TODO expose protocol
-    let protocol = 0;
+    let ty = sock as isize | flags.0;
+    let protocol = protocol.map(|proto| proto as isize).unwrap_or(0);
     let res = unsafe {
         syscall!(
             Syscalls::Socket,
@@ -101,7 +141,7 @@ pub fn socket(addr: SocketAddr, sock: SockType, flags: SockFlags) -> io::Result<
 
 #[inline]
 pub fn bind<F: AsRawFd>(socket: F, addr: SocketAddr) -> io::Result<()> {
-    let (addr, addr_len) = socket_addr(addr);
+    let (addr, addr_len) = socket_addr(&addr);
     let res = unsafe {
         syscall!(
             Syscalls::Bind,
@@ -116,7 +156,7 @@ pub fn bind<F: AsRawFd>(socket: F, addr: SocketAddr) -> io::Result<()> {
 #[inline]
 pub fn getsockname<F: AsRawFd>(socket: F) -> io::Result<SocketAddr> {
     let mut address = MaybeUninit::<libc::sockaddr_storage>::uninit();
-    let mut address_len = mem::size_of_val(&address);
+    let mut address_len = mem::size_of::<libc::sockaddr_storage>();
     let res = unsafe {
         syscall!(
             Syscalls::Getsockname,
@@ -127,8 +167,8 @@ pub fn getsockname<F: AsRawFd>(socket: F) -> io::Result<SocketAddr> {
     };
     let address = unsafe { address.assume_init() };
     result!(res).map(|_| match address.ss_family as libc::c_int {
-        AF_INET => SocketAddr::V4(unsafe { ptr::read(&address as *const _ as _) }),
-        AF_INET6 => SocketAddr::V6(unsafe { ptr::read(&address as *const _ as _) }),
+        libc::AF_INET => SocketAddr::V4(unsafe { ptr::read(&address as *const _ as _) }),
+        libc::AF_INET6 => SocketAddr::V6(unsafe { ptr::read(&address as *const _ as _) }),
         _ => unreachable!(),
     })
 }
@@ -141,7 +181,7 @@ pub fn sendmsg<F: AsRawFd>(
     msg: &[u8],
     flags: u32,
 ) -> io::Result<usize> {
-    let (name, namelen) = socket_addr(addr);
+    let (name, namelen) = socket_addr(&addr);
     let mut iov = libc::iovec {
         iov_base: msg.as_ptr() as *const _ as *mut _,
         iov_len: msg.len(),
@@ -210,22 +250,21 @@ pub fn recvmsg<F: AsRawFd>(
     };
     let address = unsafe { address.assume_init() };
     let address = match address.ss_family as libc::c_int {
-        AF_INET => SocketAddr::V4(unsafe { ptr::read(&address as *const _ as _) }),
-        AF_INET6 => SocketAddr::V6(unsafe { ptr::read(&address as *const _ as _) }),
+        libc::AF_INET => SocketAddr::V4(unsafe { ptr::read(&address as *const _ as _) }),
+        libc::AF_INET6 => SocketAddr::V6(unsafe { ptr::read(&address as *const _ as _) }),
         _ => unreachable!(),
     };
     Ok((n, address))
 }
 
 #[derive(Clone, Copy, Debug)]
-#[repr(i32)]
 pub enum SockOptLevel {
-    Socket = libc::SOL_SOCKET,
-    Ip4 = libc::SOL_IP,
-    Ip6 = libc::SOL_IPV6,
-    Udp = libc::SOL_UDP,
-    Tcp = libc::SOL_TCP,
-    Icmp6 = libc::SOL_ICMPV6,
+    Socket = libc::SOL_SOCKET as isize,
+    Ip4 = libc::SOL_IP as isize,
+    Ip6 = libc::SOL_IPV6 as isize,
+    Udp = libc::SOL_UDP as isize,
+    Tcp = libc::SOL_TCP as isize,
+    Icmp6 = libc::SOL_ICMPV6 as isize,
 }
 
 #[inline]
@@ -240,7 +279,7 @@ pub fn getsockopt<F: AsRawFd, T>(
         syscall!(
             Syscalls::Getsockopt,
             socket.as_raw_fd() as isize,
-            level as i32 as isize,
+            level as isize,
             name as isize,
             val as *mut _ as isize,
             &mut len as *mut _ as isize,
@@ -277,8 +316,8 @@ mod tests {
     #[test]
     fn test_socket_ip4_no_flags() {
         let ip4 = "127.0.0.1:0".parse().unwrap();
-        let fd1 = socket(ip4, SockType::Datagram, SockFlags::new()).unwrap();
-        let fd2 = socket(ip4, SockType::Datagram, SockFlags::new()).unwrap();
+        let fd1 = socket(AddressFamily::Inet, SockType::Datagram, SockFlags::new(), None).unwrap();
+        let fd2 = socket(AddressFamily::Inet, SockType::Datagram, SockFlags::new(), None).unwrap();
         bind(&fd1, ip4).unwrap();
         bind(&fd2, ip4).unwrap();
         let addr1 = getsockname(&fd1).unwrap();
@@ -296,8 +335,8 @@ mod tests {
     fn test_socket_ip6_with_flags() {
         let ip6 = "[::1]:0".parse().unwrap();
         let flags = SockFlags::new().cloexec().nonblock();
-        let fd1 = socket(ip6, SockType::Datagram, flags).unwrap();
-        let fd2 = socket(ip6, SockType::Datagram, flags).unwrap();
+        let fd1 = socket(AddressFamily::Inet6, SockType::Datagram, flags, None).unwrap();
+        let fd2 = socket(AddressFamily::Inet6, SockType::Datagram, flags, None).unwrap();
         bind(&fd1, ip6).unwrap();
         bind(&fd2, ip6).unwrap();
         let addr1 = getsockname(&fd1).unwrap();
@@ -312,8 +351,7 @@ mod tests {
 
     #[test]
     fn test_opts_ip4() {
-        let ip4 = "127.0.0.1:0".parse().unwrap();
-        let fd1 = socket(ip4, SockType::Datagram, SockFlags::new()).unwrap();
+        let fd1 = socket(AddressFamily::Inet, SockType::Datagram, SockFlags::new(), None).unwrap();
         let on: libc::c_int = 1;
         setsockopt(&fd1, SockOptLevel::Ip4, libc::IP_RECVTOS, &on).unwrap();
         let mut on: libc::c_int = 0;
@@ -324,8 +362,7 @@ mod tests {
     #[test]
     #[ignore]
     fn test_opts_ip6() {
-        let ip6 = "[::1]:0".parse().unwrap();
-        let fd1 = socket(ip6, SockType::Datagram, SockFlags::new()).unwrap();
+        let fd1 = socket(AddressFamily::Inet6, SockType::Datagram, SockFlags::new(), None).unwrap();
         let on: libc::c_int = 1;
         setsockopt(&fd1, SockOptLevel::Ip6, libc::IPV6_RECVTCLASS, &on).unwrap();
         let mut on: libc::c_int = 0;
